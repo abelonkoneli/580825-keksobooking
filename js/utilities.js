ecf61b62fae8fb24b4f3e8d87180756f01b1a484
abelonkoneli <abelonkoneli@gmail.com>
2018-03-01 06:45:05 +0200

    Добавлены хендлеры для обработки успешных и неуспешных запросов

--- js/utilities.js
+++ js/utilities.js
@@ -39,7 +39,6 @@
     enableNoticeForm: function () {
       if (window.data.noticeForm.classList.contains('notice__form--disabled')) {
         window.data.noticeForm.classList.remove('notice__form--disabled');
-        window.createArray();
         if (window.data.mapElement.classList.contains('map--faded')) {
           window.data.mapElement.classList.remove('map--faded');
         }
@@ -50,8 +49,24 @@
         }
         window.data.noticeSubmitBtn.removeAttribute('disabled');
         window.data.noticeAddress.setAttribute('readonly', true);
-        window.insertButtons();
+        // window.insertButtons();
+        window.backend.load(window.utilities.successLoadHandler, window.utilities.errorLoadHandler);
       }
+    },
+    successLoadHandler: function (data) {
+      window.insertButtons(data);
+    },
+
+    errorLoadHandler: function (errorMessage) {
+      var errorLoadAlert = document.createElement('div');
+      errorLoadAlert.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+      errorLoadAlert.style.position = 'absolute';
+      errorLoadAlert.style.left = 0;
+      errorLoadAlert.style.right = 0;
+      errorLoadAlert.style.fontSize = '30px';
+
+      errorLoadAlert.textContent = errorMessage;
+      document.body.insertAdjacentElement('afterbegin', errorLoadAlert);
     },
 
     disableNoticeForm: function () {
@@ -76,7 +91,56 @@
       if (evt.keyCode === window.data.ESC_KEYCODE) {
         window.utilities.removeCardElement();
       }
-    }
+    },
+    successSendHandler: function () {
+      window.utilities.disableNoticeForm();
+      var pinsCollection = window.data.mapElement.querySelectorAll('.map__pin:not(.map__pin--main)');
+      for (var i = 0; i < pinsCollection.length; i++) {
+        document.querySelector('.map__pins').removeChild(pinsCollection[i]);
+      }
+      window.data.noticeAddress.value = (window.data.pinMain.offsetLeft + 50 / 2) + ', ' + (window.data.pinMain.offsetTop + (70 + 22) / 2);
+      window.data.pinMain.style = '';
+      window.data.noticeTitle.value = '';
+      window.data.noticePrice.value = '';
+      window.data.noticeForm.querySelector('#description').value = '';
+      var checkboxes = window.data.noticeForm.querySelectorAll('input[type = "checkbox"]');
+      for (var n = 0; n < checkboxes.length; n++) {
+        checkboxes[n].checked = false;
+      }
+      var selects = window.data.noticeForm.querySelectorAll('select');
+      for (var j = 0; j < selects.length; j++) {
+        selects[j].selectedIndex = 0;
+      }
+    },
 
+    errorSendHandler: function (errorMessage) {
+      window.utilities.disableNoticeForm();
+      var pinsCollection = window.data.mapElement.querySelectorAll('.map__pin:not(.map__pin--main)');
+      for (var i = 0; i < pinsCollection.length; i++) {
+        document.querySelector('.map__pins').removeChild(pinsCollection[i]);
+      }
+      window.data.noticeAddress.value = (window.data.pinMain.offsetLeft + 50 / 2) + ', ' + (window.data.pinMain.offsetTop + (70 + 22) / 2);
+      window.data.pinMain.style = '';
+      window.data.noticeTitle.value = '';
+      window.data.noticePrice.value = '';
+      window.data.noticeForm.querySelector('#description').value = '';
+      var checkboxes = window.data.noticeForm.querySelectorAll('input[type = "checkbox"]');
+      for (var n = 0; n < checkboxes.length; n++) {
+        checkboxes[n].checked = false;
+      }
+      var selects = window.data.noticeForm.querySelectorAll('select');
+      for (var j = 0; j < selects.length; j++) {
+        selects[j].selectedIndex = 0;
+      }
+      var errorSendAlert = document.createElement('div');
+      errorSendAlert.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+      errorSendAlert.style.position = 'absolute';
+      errorSendAlert.style.left = 0;
+      errorSendAlert.style.right = 0;
+      errorSendAlert.style.fontSize = '30px';
+
+      errorSendAlert.textContent = errorMessage;
+      window.data.noticeForm.insertAdjacentElement('afterbegin', errorSendAlert);
+    }
   };
 })();
