22f1cea3725d92039b397ecaff423c4f5946110f
abelonkoneli <abelonkoneli@gmail.com>
2018-03-02 14:13:06 +0200

    Ограничение количества меток на карте

--- js/map.js
+++ js/map.js
@@ -21,10 +21,14 @@
   };
   window.data.pinMain.addEventListener('mouseup', window.utilities.enableNoticeForm);
   window.insertButtons = function (data) {
+    var pinsColl = document.querySelectorAll('.map__pin:not(.map__pin--main)');
+    for (var i = 0; i < pinsColl; i++) {
+      document.querySelector('.map__pins').removeChild(pinsColl[i]);
+    }
     var fragment = document.createDocumentFragment();
     var btn;
     window.utilities.sortArray(data);
-    for (var i = 0; i < 8; i++) {
+    for (i = 0; i < Math.min(5, data.length); i++) {
       btn = window.createButtonElement(data[i]);
       buttonClickHandlerOuter(btn, data[i]);
       fragment.appendChild(btn);
