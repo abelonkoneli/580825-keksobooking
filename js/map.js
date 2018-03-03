b1d74f11b4d90d92276faef1778e51d4e6913624
abelonkoneli <abelonkoneli@gmail.com>
2018-03-03 11:52:09 +0200

    Проверка по критериям

--- js/map.js
+++ js/map.js
@@ -2,35 +2,36 @@
 'use strict';
 
 (function () {
-
-  var insertCardElement = function (prop) {
+  var MAX_PINS = 5;
+  var insertCardElement = function (proposal) {
     if (window.data.mapElement.contains(window.data.mapElement.querySelector('.map__card'))) {
       window.utilities.removeCardElement();
     }
-    window.data.mapElement.insertBefore(window.createCardElement(prop), window.data.mapElement.querySelector('.map__filters-container'));
+    window.data.mapElement.insertBefore(window.createCardElement(proposal), window.data.mapElement.querySelector('.map__filters-container'));
     document.addEventListener('keydown', window.utilities.cardEscKeydownHandler);
     var cardCloseElement = document.querySelector('.popup__close');
     cardCloseElement.addEventListener('click', window.utilities.removeCardElement);
   };
 
-  var buttonClickHandlerOuter = function (btn, prop) {
-    var buttonPinClickHandlerInner = function () {
-      insertCardElement(prop);
+  var buttonClickOuterHandler = function (btn, proposal) {
+    var buttonClickInnerHandler = function () {
+      insertCardElement(proposal);
     };
-    return btn.addEventListener('click', buttonPinClickHandlerInner);
+    return btn.addEventListener('click', buttonClickInnerHandler);
   };
-  window.data.pinMain.addEventListener('mouseup', window.utilities.enableNoticeForm);
+
+  window.data.pinMainElement.addEventListener('mouseup', window.utilities.enableNoticeForm);
   window.insertButtons = function (data) {
-    var pinsColl = document.querySelectorAll('.map__pin:not(.map__pin--main)');
-    for (var i = 0; i < pinsColl; i++) {
-      document.querySelector('.map__pins').removeChild(pinsColl[i]);
+    var pinCollection = document.querySelectorAll('.map__pin:not(.map__pin--main)');
+    for (var i = 0; i < pinCollection; i++) {
+      document.querySelector('.map__pins').removeChild(pinCollection[i]);
     }
     var fragment = document.createDocumentFragment();
     var btn;
     window.utilities.sortArray(data);
-    for (i = 0; i < Math.min(5, data.length); i++) {
+    for (i = 0; i < Math.min(MAX_PINS, data.length); i++) {
       btn = window.createButtonElement(data[i]);
-      buttonClickHandlerOuter(btn, data[i]);
+      buttonClickOuterHandler(btn, data[i]);
       fragment.appendChild(btn);
     }
     return document.querySelector('.map__pins').appendChild(fragment);
