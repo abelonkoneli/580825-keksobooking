880bf608099465285310c266be3660b6556a9ebc
abelonkoneli <abelonkoneli@gmail.com>
2018-03-03 11:52:41 +0200

    Проверка по критериям

--- js/utilities.js
+++ js/utilities.js
@@ -2,28 +2,53 @@
 'use strict';
 
 (function () {
-  var pinMainProps = {
+  var PinMainValues = {
     WIDTH: 65,
     HEIGHT: 65,
     AFTER_HEIGHT: 22,
     Z_INDEX: 999
   };
+
   var DEBOUNCE_INTERVAL = 500;
   var lastTimeout;
+  var ERROR_CLOSE_TIMEOUT = 1700;
+
+  var resetFields = function (el) {
+    var checkboxes = el.querySelectorAll('input[type = "checkbox"]');
+    for (var i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = false;
+    }
+    var selects = el.querySelectorAll('select');
+    for (i = 0; i < selects.length; i++) {
+      selects[i].selectedIndex = 0;
+    }
+  };
+
+  var updateNoticeFields = function () {
+    window.data.pinMainElement.style = '';
+    window.data.Notice.titleElement.value = '';
+    window.data.Notice.priceElement.value = '';
+    window.data.Notice.formElement.querySelector('#description').value = '';
+    resetFields(window.data.Notice.formElement);
+  };
+
+  var stylizeError = function (el, errorMessage) {
+    el.style = 'z-index: ' + PinMainValues.Z_INDEX + '; ' + 'margin: 0 auto; text-align: center; background-color: white;';
+    el.style.position = 'fixed';
+    el.style.left = '0';
+    el.style.right = '0';
+    el.style.top = '45%';
+    el.style.bottom = '48%';
+    el.style.fontSize = '30px';
+    el.textContent = errorMessage;
+    setTimeout(function () {
+      el.style.display = 'none';
+    },
+    ERROR_CLOSE_TIMEOUT);
+    return el;
+  };
 
   window.utilities = {
-    checkType: function (key) {
-      for (var i = 0; i < window.data.titles.length; i++) {
-        if (key === 'Большая уютная квартира' || key === 'Маленькая неуютная квартира') {
-          window.data.type = 'flat';
-        } else if (key === 'Огромный прекрасный дворец' || key === 'Маленький ужасный дворец' || key === 'Красивый гостевой домик' || key === 'Некрасивый негостеприимный домик') {
-          window.data.type = 'house';
-        } else {
-          window.data.type = 'bungalo';
-        }
-      } return window.data.type;
-    },
-
     getRandomQuantity: function (max, min) {
       return (Math.round(Math.random() * (max - min) + min));
     },
@@ -39,50 +64,37 @@
     },
 
     enableNoticeForm: function () {
-      if (window.data.noticeForm.classList.contains('notice__form--disabled')) {
-        window.data.noticeForm.classList.remove('notice__form--disabled');
+      if (window.data.Notice.formElement.classList.contains('notice__form--disabled')) {
+        window.data.Notice.formElement.classList.remove('notice__form--disabled');
         if (window.data.mapElement.classList.contains('map--faded')) {
           window.data.mapElement.classList.remove('map--faded');
         }
-        window.data.noticeAddress.value = ((window.data.pinMain.offsetLeft + pinMainProps.WIDTH / 2) + ', ' + (window.data.pinMain.offsetTop + pinMainProps.HEIGHT + pinMainProps.AFTER_HEIGHT));
-        window.data.pinMain.style.zIndex = pinMainProps.Z_INDEX;
-        for (var i = 0; i < window.data.noticeCollection.length; i++) {
-          window.data.noticeCollection[i].removeAttribute('disabled');
+        for (var i = 0; i < window.data.Notice.collection.length; i++) {
+          window.data.Notice.collection[i].removeAttribute('disabled');
         }
-        window.data.noticeSubmitBtn.removeAttribute('disabled');
-        window.data.noticeAddress.setAttribute('readonly', true);
-        // window.insertButtons();
+        window.data.Notice.submitBtnElement.removeAttribute('disabled');
+        window.data.Notice.addressElement.setAttribute('readonly', true);
         window.backend.load(window.utilities.successLoadHandler, window.utilities.errorLoadHandler);
+        window.data.Notice.addressElement.value = ((window.data.pinMainElement.offsetLeft + PinMainValues.WIDTH / 2) + ', ' + (window.data.pinMainElement.offsetTop + PinMainValues.HEIGHT + PinMainValues.AFTER_HEIGHT));
+        window.data.pinMainElement.style.zIndex = PinMainValues.Z_INDEX;
       }
-    },
-    successLoadHandler: function (data) {
-      window.optionalOffers = data;
-      window.insertButtons(data);
-    },
-
-    errorLoadHandler: function (errorMessage) {
-      var errorLoadAlert = document.createElement('div');
-      errorLoadAlert.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-      errorLoadAlert.style.position = 'absolute';
-      errorLoadAlert.style.left = 0;
-      errorLoadAlert.style.right = 0;
-      errorLoadAlert.style.fontSize = '30px';
-
-      errorLoadAlert.textContent = errorMessage;
-      document.body.insertAdjacentElement('afterbegin', errorLoadAlert);
     },
 
     disableNoticeForm: function () {
-      window.data.noticeAddress.value = (window.data.pinMain.offsetLeft + pinMainProps.WIDTH / 2) + ', ' + (window.data.pinMain.offsetTop + (pinMainProps.HEIGHT + pinMainProps.AFTER_HEIGHT) / 2);
-      for (var i = 0; i < window.data.noticeCollection.length; i++) {
-        window.data.noticeCollection[i].setAttribute('disabled', true);
+      window.utilities.removePinCollection();
+      if (window.data.mapElement.contains(document.querySelector('.map__card'))) {
+        window.utilities.removeCardElement();
       }
+      updateNoticeFields();
+      resetFields(window.data.mapFiltersElement);
+      for (var i = 0; i < window.data.Notice.collection.length; i++) {
+        window.data.Notice.collection[i].setAttribute('disabled', true);
+      }
+      window.data.Notice.submitBtnElement.setAttribute('disabled', true);
+      window.data.Notice.formElement.classList.add('notice__form--disabled');
       document.querySelector('.form__element--submit').removeAttribute('disabled');
-      document.querySelector('.form__submit').setAttribute('disabled', true);
-      window.data.noticeForm.classList.add('notice__form--disabled');
-      if (!window.data.mapElement.classList.contains('map--faded')) {
-        window.data.mapElement.classList.add('map--faded');
-      }
+      window.data.mapElement.classList.add('map--faded');
+      window.data.Notice.addressElement.value = (window.data.pinMainElement.offsetLeft + PinMainValues.WIDTH / 2) + ', ' + (window.data.pinMainElement.offsetTop + (PinMainValues.HEIGHT + PinMainValues.AFTER_HEIGHT) / 2);
     },
 
     removeCardElement: function () {
@@ -91,65 +103,46 @@
     },
 
     cardEscKeydownHandler: function (evt) {
-      if (evt.keyCode === window.data.ESC_KEYCODE) {
+      if (evt.keyCode === window.data.Keycode.ESC) {
         window.utilities.removeCardElement();
       }
     },
+
+    successLoadHandler: function (data) {
+      window.optionalOffers = data;
+      window.insertButtons(data);
+    },
+
+    errorLoadHandler: function (errorMessage) {
+      var errorLoadAlert = document.createElement('div');
+      stylizeError(errorLoadAlert, errorMessage);
+      document.body.insertAdjacentElement('afterbegin', errorLoadAlert);
+      window.utilities.disableNoticeForm();
+    },
+
     successSendHandler: function () {
       window.utilities.disableNoticeForm();
-      var pinsCollection = window.data.mapElement.querySelectorAll('.map__pin:not(.map__pin--main)');
-      for (var i = 0; i < pinsCollection.length; i++) {
-        document.querySelector('.map__pins').removeChild(pinsCollection[i]);
-      }
-      window.data.noticeAddress.value = (window.data.pinMain.offsetLeft + 50 / 2) + ', ' + (window.data.pinMain.offsetTop + (70 + 22) / 2);
-      window.data.pinMain.style = '';
-      window.data.noticeTitle.value = '';
-      window.data.noticePrice.value = '';
-      window.data.noticeForm.querySelector('#description').value = '';
-      var checkboxes = window.data.noticeForm.querySelectorAll('input[type = "checkbox"]');
-      for (var n = 0; n < checkboxes.length; n++) {
-        checkboxes[n].checked = false;
-      }
-      var selects = window.data.noticeForm.querySelectorAll('select');
-      for (var j = 0; j < selects.length; j++) {
-        selects[j].selectedIndex = 0;
-      }
     },
 
     errorSendHandler: function (errorMessage) {
-      window.utilities.disableNoticeForm();
-      var pinsCollection = window.data.mapElement.querySelectorAll('.map__pin:not(.map__pin--main)');
-      for (var i = 0; i < pinsCollection.length; i++) {
-        document.querySelector('.map__pins').removeChild(pinsCollection[i]);
-      }
-      window.data.noticeAddress.value = (window.data.pinMain.offsetLeft + 50 / 2) + ', ' + (window.data.pinMain.offsetTop + (70 + 22) / 2);
-      window.data.pinMain.style = '';
-      window.data.noticeTitle.value = '';
-      window.data.noticePrice.value = '';
-      window.data.noticeForm.querySelector('#description').value = '';
-      var checkboxes = window.data.noticeForm.querySelectorAll('input[type = "checkbox"]');
-      for (var n = 0; n < checkboxes.length; n++) {
-        checkboxes[n].checked = false;
-      }
-      var selects = window.data.noticeForm.querySelectorAll('select');
-      for (var j = 0; j < selects.length; j++) {
-        selects[j].selectedIndex = 0;
-      }
       var errorSendAlert = document.createElement('div');
-      errorSendAlert.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-      errorSendAlert.style.position = 'absolute';
-      errorSendAlert.style.left = 0;
-      errorSendAlert.style.right = 0;
-      errorSendAlert.style.fontSize = '30px';
-
-      errorSendAlert.textContent = errorMessage;
-      window.data.noticeForm.insertAdjacentElement('afterbegin', errorSendAlert);
+      stylizeError(errorSendAlert, errorMessage);
+      window.data.Notice.formElement.insertAdjacentElement('afterend', errorSendAlert);
+      window.utilities.disableNoticeForm();
     },
-    debounce: function (fun) {
+
+    removePinCollection: function () {
+      var pinCollection = window.data.mapElement.querySelectorAll('.map__pin:not(.map__pin--main)');
+      for (var i = 0; i < pinCollection.length; i++) {
+        document.querySelector('.map__pins').removeChild(pinCollection[i]);
+      }
+    },
+
+    debounce: function (cb) {
       if (lastTimeout) {
         window.clearTimeout(lastTimeout);
       }
-      lastTimeout = window.setTimeout(fun, DEBOUNCE_INTERVAL);
+      lastTimeout = window.setTimeout(cb, DEBOUNCE_INTERVAL);
     }
   };
 })();
