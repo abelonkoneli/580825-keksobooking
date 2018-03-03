acfa8fbb93f85bc8324ef6c9005831838d078085
abelonkoneli <abelonkoneli@gmail.com>
2018-03-03 11:47:28 +0200

    Проверка по критериям

--- js/button.js
+++ js/button.js
@@ -2,14 +2,14 @@
 'use strict';
 
 (function () {
-  var buttonTemplate = document.querySelector('template').content.querySelector('.map__pin');
+  var buttonElementTemplate = document.querySelector('template').content.querySelector('.map__pin');
   var BTN_WIDTH = 50;
   var BTN_HEIGHT = 70;
-  window.createButtonElement = function (prop) {
-    var buttonElement = buttonTemplate.cloneNode(true);
-    buttonElement.style.left = (prop.location.x + BTN_WIDTH / 2) + 'px';
-    buttonElement.style.top = (prop.location.y + BTN_HEIGHT) + 'px';
-    buttonElement.querySelector('img').setAttribute('src', prop.author.avatar);
+  window.createButtonElement = function (proposal) {
+    var buttonElement = buttonElementTemplate.cloneNode(true);
+    buttonElement.style.left = (proposal.location.x + BTN_WIDTH / 2) + 'px';
+    buttonElement.style.top = (proposal.location.y + BTN_HEIGHT) + 'px';
+    buttonElement.querySelector('img').setAttribute('src', proposal.author.avatar);
     return buttonElement;
   };
 })();
