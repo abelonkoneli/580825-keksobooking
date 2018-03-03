4c92456ea12e0ffe3933e76fcf04a2fd2031eaab
abelonkoneli <abelonkoneli@gmail.com>
2018-03-03 11:51:51 +0200

    Проверка по критериям

--- js/form.js
+++ js/form.js
@@ -1,59 +1,79 @@
-// модуль описывает поведение формы notice__form
+// модуль описывает поведение формы notice__time
+
 'use strict';
 
 (function () {
 
   window.utilities.disableNoticeForm();
 
-  var roomsCapacityClickHandler = function () {
-    if (+window.data.noticeRoomNumber.value < +window.data.noticeCapacity.value) {
-      window.data.noticeCapacity.setCustomValidity('Гостей не может быть больше, чем комнат');
-    } else if (window.data.noticeRoomNumber.value === '100' && (window.data.noticeCapacity.value !== '0')) {
-      window.data.noticeCapacity.setCustomValidity('Сто комнат не рассчитаны на гостей');
-    } else if (window.data.noticeRoomNumber.value !== '100' && window.data.noticeCapacity.value === '0') {
-      window.data.noticeCapacity.setCustomValidity('Гостей не может быть меньше одного');
-    } else {
-      window.data.noticeCapacity.setCustomValidity('');
-    }
-  };
-  var priceClickHandler = function (evt) {
+  var handleInvalidity = function (evt) {
     if (!evt.invalid) {
-      window.data.noticePrice.classList.remove('bordered');
-    }
-    var typeMinPrices = {
-      bungalo: '0',
-      flat: '1000',
-      house: '5000',
-      palace: '10000'
-    };
-    if (window.data.noticeType.value === window.data.types[0]) {
-      window.data.noticePrice.min = typeMinPrices.flat;
-    } else if (window.data.noticeType.value === window.data.types[2]) {
-      window.data.noticePrice.min = typeMinPrices.bungalo;
-    } else if (window.data.noticeType.value === window.data.types[1]) {
-      window.data.noticePrice.min = typeMinPrices.house;
-    } else if (window.data.noticeType.value === window.data.types[3]) {
-      window.data.noticePrice.min = typeMinPrices.palace;
+      evt.target.classList.remove('bordered');
     }
   };
+
   var titleClickHandler = function (evt) {
-    if (!evt.invalid) {
-      window.data.noticeTitle.classList.remove('bordered');
-    }
+    handleInvalidity(evt);
   };
+
   var capacityClickHandler = function (evt) {
-    if (!evt.invalid) {
-      window.data.noticeCapacity.classList.remove('bordered');
+    handleInvalidity(evt);
+  };
+
+  var roomsCapacityClickHandler = function () {
+    if (+window.data.Notice.roomNumberElement.value < +window.data.Notice.capacityElement.value) {
+      window.data.Notice.capacityElement.setCustomValidity('Гостей не может быть больше, чем комнат');
+    } else if (+window.data.Notice.roomNumberElement.value === '100' && +window.data.Notice.capacityElement.value !== '0') {
+      window.data.Notice.capacityElement.setCustomValidity('Сто комнат не рассчитаны на гостей');
+    } else if (+window.data.Notice.roomNumberElement.value !== '100' && +window.data.Notice.capacityElement.value === '0') {
+      window.data.Notice.capacityElement.setCustomValidity('Гостей не может быть меньше одного');
+    } else {
+      window.data.Notice.capacityElement.setCustomValidity('');
     }
   };
+
+  var typePriceClickHandler = function (evt) {
+    var typeMinPrices = [
+      {
+        TYPE: 'bungalo',
+        PRICE: '0'
+      },
+      {
+        TYPE: 'flat',
+        PRICE: '1000'
+      },
+      {
+        TYPE: 'house',
+        PRICE: '5000'
+      },
+      {
+        TYPE: 'palace',
+        PRICE: '10000'
+      }
+    ];
+
+    if (window.data.Notice.typeElement.value === typeMinPrices[1].TYPE) {
+      window.data.Notice.priceElement.min = typeMinPrices[1].PRICE;
+    } else if (window.data.Notice.typeElement.value === typeMinPrices[2].TYPE) {
+      window.data.Notice.priceElement.min = typeMinPrices[2].PRICE;
+    } else if (window.data.Notice.typeElement.value === typeMinPrices[3].TYPE) {
+      window.data.Notice.priceElement.min = typeMinPrices[3].PRICE;
+    } else {
+      window.data.Notice.priceElement.min = typeMinPrices[0].PRICE;
+    }
+    handleInvalidity(evt);
+  };
+
   var timeInClickHandler = function () {
-    window.data.noticeTimeOut.value = window.data.noticeTimeIn.value;
-    window.data.noticeTimeIn.title = 'Обратите внимание: при изменении времени заезда меняется время выезда и наоборот';
+    window.data.Notice.timeOutElement.value = window.data.Notice.timeInElement.value;
+    window.data.Notice.timeInElement.title = 'Обратите внимание: при изменении времени заезда меняется время выезда и наоборот';
   };
+
   var timeOutClickHandler = function () {
-    window.data.noticeTimeIn.value = window.data.noticeTimeOut.value;
-    window.data.noticeTimeOut.title = 'Обратите внимание: при изменении времени выезда меняется время заезда и наоборот';
+    window.data.Notice.timeInElement.value = window.data.Notice.timeOutElement.value;
+    window.data.Notice.timeOutElement.title = 'Обратите внимание: при изменении времени выезда меняется время заезда и наоборот';
   };
+
   var submitInvalidClickHandler = function () {
     roomsCapacityClickHandler();
     var invalidCollection = document.querySelectorAll('input:invalid, select:invalid');
@@ -61,35 +81,30 @@
       invalidCollection[i].classList.add('bordered');
     }
   };
+
   var resetClickHandler = function () {
-    if (document.querySelector('.map__card')) {
-      window.utilities.removeCardElement();
-    }
-    var pinsCollection = window.data.mapElement.querySelectorAll('.map__pin:not(.map__pin--main)');
-    for (var i = 0; i < pinsCollection.length; i++) {
-      document.querySelector('.map__pins').removeChild(pinsCollection[i]);
-    }
     window.utilities.disableNoticeForm();
-    window.data.noticeAddress.value = (window.data.pinMain.offsetLeft + 50 / 2) + ', ' + (window.data.pinMain.offsetTop + (70 + 22) / 2);
-    window.data.pinMain.style = '';
   };
+
   var resetKeypressHandler = function (evt) {
-    if (evt.keycode === window.data.ENTER_KEYCODE) {
+    if (evt.keycode === window.data.Keycode.ENTER) {
       resetClickHandler();
     }
   };
-  window.data.noticeSubmitBtn.addEventListener('click', submitInvalidClickHandler);
-  window.data.noticeCapacity.addEventListener('click', capacityClickHandler);
-  window.data.noticeTitle.addEventListener('click', titleClickHandler);
-  window.data.noticePrice.addEventListener('click', priceClickHandler);
-  window.data.noticeTimeIn.addEventListener('click', timeInClickHandler);
-  window.data.noticeTimeOut.addEventListener('click', timeOutClickHandler);
-  window.data.noticeCapacity.addEventListener('click', roomsCapacityClickHandler);
-  window.data.noticeRoomNumber.addEventListener('click', roomsCapacityClickHandler);
-  window.data.noticeReset.addEventListener('click', resetClickHandler);
-  window.data.noticeReset.addEventListener('keypress', resetKeypressHandler);
-  window.data.noticeForm.addEventListener('submit', function (evt) {
+
+  window.data.Notice.submitBtnElement.addEventListener('click', submitInvalidClickHandler);
+  window.data.Notice.capacityElement.addEventListener('click', capacityClickHandler);
+  window.data.Notice.capacityElement.addEventListener('click', roomsCapacityClickHandler);
+  window.data.Notice.roomNumberElement.addEventListener('click', roomsCapacityClickHandler);
+  window.data.Notice.titleElement.addEventListener('click', titleClickHandler);
+  window.data.Notice.priceElement.addEventListener('click', typePriceClickHandler);
+  window.data.Notice.typeElement.addEventListener('click', typePriceClickHandler);
+  window.data.Notice.timeInElement.addEventListener('click', timeInClickHandler);
+  window.data.Notice.timeOutElement.addEventListener('click', timeOutClickHandler);
+  window.data.Notice.resetElement.addEventListener('click', resetClickHandler);
+  window.data.Notice.resetElement.addEventListener('keypress', resetKeypressHandler);
+  window.data.Notice.formElement.addEventListener('submit', function (evt) {
     evt.preventDefault();
-    window.backend.send(new FormData(window.data.noticeForm), window.utilities.successSendHandler, window.utilities.errorSendHandler);
+    window.backend.send(new FormData(window.data.Notice.formElement), window.utilities.successSendHandler, window.utilities.errorSendHandler);
   });
 })();
