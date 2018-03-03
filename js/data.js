8985d1026b92252b8a13c7d5c84b7254171723b4
abelonkoneli <abelonkoneli@gmail.com>
2018-03-03 11:48:26 +0200

    Проверка по критериям

--- js/data.js
+++ js/data.js
@@ -3,47 +3,27 @@
 
 (function () {
   window.data = {
-    proposals: [],
-    titles: [
-      'Большая уютная квартира',
-      'Маленькая неуютная квартира',
-      'Огромный прекрасный дворец',
-      'Маленький ужасный дворец',
-      'Красивый гостевой домик',
-      'Некрасивый негостеприимный домик',
-      'Уютное бунгало далеко от моря',
-      'Неуютное бунгало по колено в воде'
-    ],
-    featuresArrCopy: [
-      'wifi',
-      'dishwasher',
-      'parking',
-      'washer',
-      'elevator',
-      'conditioner'
-    ],
-    types: [
-      'flat',
-      'house',
-      'bungalo',
-      'palace'
-    ],
-    type: [],
-    ESC_KEYCODE: 27,
-    ENTER_KEYCODE: 13,
+    Keycode: {
+      ESC: 27,
+      ENTER: 13
+    },
     mapElement: document.querySelector('.map'),
-    noticeForm: document.querySelector('.notice__form'),
-    noticeCollection: document.querySelectorAll('fieldset:not(#housing-features)'),
-    noticeAddress: document.querySelector('#address'),
-    noticeTimeIn: document.querySelector('#timein'),
-    noticeTimeOut: document.querySelector('#timeout'),
-    pinMain: document.querySelector('.map__pin--main'),
-    noticeRoomNumber: document.querySelector('#room_number'),
-    noticeReset: document.querySelector('.form__reset'),
-    noticeSubmitBtn: document.querySelector('.form__submit'),
-    noticeType: document.querySelector('#type'),
-    noticeCapacity: document.querySelector('#capacity'),
-    noticePrice: document.querySelector('#price'),
-    noticeTitle: document.querySelector('#title')
+    pinMainElement: document.querySelector('.map__pin--main'),
+    mapFiltersElement: document.querySelector('.map__filters'),
+    Notice: {
+      formElement: document.querySelector('.notice__form'),
+      collection: document.querySelectorAll('fieldset:not(#housing-features)'),
+      addressElement: document.querySelector('#address'),
+      timeInElement: document.querySelector('#timein'),
+      timeOutElement: document.querySelector('#timeout'),
+      roomNumberElement: document.querySelector('#room_number'),
+      resetElement: document.querySelector('.form__reset'),
+      submitBtnElement: document.querySelector('.form__submit'),
+      typeElement: document.querySelector('#type'),
+      capacityElement: document.querySelector('#capacity'),
+      priceElement: document.querySelector('#price'),
+      titleElement: document.querySelector('#title')
+    }
+
   };
 })();
