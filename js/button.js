90ae8e897fde95d6786de23e81850e0e1e1c2873
abelonkoneli <abelonkoneli@gmail.com>
2018-03-03 15:32:20 +0200

    Правки по базовым критериям

--- js/button.js
+++ js/button.js
@@ -2,9 +2,9 @@
 'use strict';
 
 (function () {
-  var buttonElementTemplate = document.querySelector('template').content.querySelector('.map__pin');
   var BTN_WIDTH = 50;
   var BTN_HEIGHT = 70;
+  var buttonElementTemplate = document.querySelector('template').content.querySelector('.map__pin');
   window.createButtonElement = function (proposal) {
     var buttonElement = buttonElementTemplate.cloneNode(true);
     buttonElement.style.left = (proposal.location.x + BTN_WIDTH / 2) + 'px';
