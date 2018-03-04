8ec5f43f164f849a8814aafbc5be94816247bd86
abelonkoneli <abelonkoneli@gmail.com>
2018-03-04 18:54:28 +0200

    Правки по критериям

--- js/backend.js
+++ js/backend.js
@@ -1,60 +1,44 @@
 'use strict';
 
 (function () {
-  var TIMEOUT = 10000;
   var Code = {
     SUCCESS: 200,
     CASHED: 304
   };
-  var url = {
-    load: 'https://js.dump.academy/keksobooking/data',
-    send: 'https://js.dump.academy/keksobooking'
+  var Url = {
+    LOAD: 'https://js.dump.academy/keksobooking/data',
+    SEND: 'https://js.dump.academy/keksobooking'
+  };
+  var TIMEOUT = 10000;
+  var treatXhr = function (xhr, onLoad, onError) {
+    xhr.addEventListener('load', function () {
+      if (xhr.status === Code.SUCCESS || xhr.status === Code.CASHED) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+    xhr.timeout = TIMEOUT;
   };
   window.backend = {
     load: function (onLoad, onError) {
       var xhr = new XMLHttpRequest();
       xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === Code.SUCCESS || xhr.status === Code.CASHED) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = TIMEOUT;
-
-      xhr.open('GET', url.load);
+      treatXhr(xhr, onLoad, onError);
+      xhr.open('GET', Url.LOAD);
       xhr.send(xhr.response);
     },
     send: function (data, onLoad, onError) {
       var xhr = new XMLHttpRequest();
       xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === Code.SUCCESS || xhr.status === Code.CASHED) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = TIMEOUT;
-
-      xhr.open('POST', url.send);
+      treatXhr(xhr, onLoad, onError);
+      xhr.open('POST', Url.SEND);
       xhr.send(data);
     }
   };
