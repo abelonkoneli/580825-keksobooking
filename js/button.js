623f20415fa56352157d93f1bd155d50eafc50b5
abelonkoneli <abelonkoneli@gmail.com>
2018-03-04 18:55:19 +0200

    Правки по критериям

--- js/button.js
+++ js/button.js
@@ -2,14 +2,14 @@
 'use strict';
 
 (function () {
-  var BTN_WIDTH = 50;
-  var BTN_HEIGHT = 70;
   var buttonElementTemplate = document.querySelector('template').content.querySelector('.map__pin');
+  var BUTTON_WIDTH = 50;
+  var BUTTON_HEIGHT = 70;
   window.createButtonElement = function (proposal) {
     var buttonElement = buttonElementTemplate.cloneNode(true);
-    buttonElement.style.left = (proposal.location.x + BTN_WIDTH / 2) + 'px';
-    buttonElement.style.top = (proposal.location.y + BTN_HEIGHT) + 'px';
-    buttonElement.querySelector('img').setAttribute('src', proposal.author.avatar);
+    buttonElement.style.left = (proposal.location.x - BUTTON_WIDTH / 2) + 'px';
+    buttonElement.style.top = (proposal.location.y - BUTTON_HEIGHT) + 'px';
+    buttonElement.querySelector('img').src = proposal.author.avatar;
     return buttonElement;
   };
 })();
