b1d95d4424c46177c033aca6c769f89059ee5fb7
abelonkoneli <abelonkoneli@gmail.com>
2018-03-04 18:57:06 +0200

    Правки по критериям

--- js/form.js
+++ js/form.js
@@ -6,29 +6,40 @@
 
   window.utilities.disableNoticeForm();
 
-  var handleInvalidity = function (evt) {
+  var manageInvalidity = function (evt) {
     if (!evt.invalid) {
       evt.target.classList.remove('bordered');
     }
   };
 
   var titleClickHandler = function (evt) {
-    handleInvalidity(evt);
+    manageInvalidity(evt);
   };
 
   var capacityClickHandler = function (evt) {
-    handleInvalidity(evt);
+    manageInvalidity(evt);
+  };
+  var guestsQuantity = {
+    MAX: 100,
+    MIN: 0
+  };
+
+  var removeBorder = function () {
+    var invalidFields = document.querySelectorAll('input:invalid, select:invalid');
+    for (var i = 0; i < invalidFields.length; i++) {
+      invalidFields[i].classList.remove('bordered');
+    }
   };
 
   var roomsCapacityClickHandler = function () {
-    if (+window.data.Notice.roomNumberElement.value < +window.data.Notice.capacityElement.value) {
-      window.data.Notice.capacityElement.setCustomValidity('Гостей не может быть больше, чем комнат');
-    } else if (+window.data.Notice.roomNumberElement.value === '100' && +window.data.Notice.capacityElement.value !== '0') {
-      window.data.Notice.capacityElement.setCustomValidity('Сто комнат не рассчитаны на гостей');
-    } else if (+window.data.Notice.roomNumberElement.value !== '100' && +window.data.Notice.capacityElement.value === '0') {
-      window.data.Notice.capacityElement.setCustomValidity('Гостей не может быть меньше одного');
+    if (+window.data.notice.roomNumberElement.value < +window.data.notice.capacityElement.value) {
+      window.data.notice.capacityElement.setCustomValidity('Гостей не может быть больше, чем комнат');
+    } else if (+window.data.notice.roomNumberElement.value === guestsQuantity.MAX && +window.data.notice.capacityElement.value !== guestsQuantity.MIN) {
+      window.data.notice.capacityElement.setCustomValidity('Сто комнат не рассчитаны на гостей');
+    } else if (+window.data.notice.roomNumberElement.value !== guestsQuantity.MAX && +window.data.notice.capacityElement.value === guestsQuantity.MIN) {
+      window.data.notice.capacityElement.setCustomValidity('Гостей не может быть меньше одного');
     } else {
-      window.data.Notice.capacityElement.setCustomValidity('');
+      window.data.notice.capacityElement.setCustomValidity('');
     }
   };
 
@@ -52,37 +63,39 @@
       }
     ];
 
-    if (window.data.Notice.typeElement.value === typeMinPrices[1].type) {
-      window.data.Notice.priceElement.min = typeMinPrices[1].price;
-    } else if (window.data.Notice.typeElement.value === typeMinPrices[2].type) {
-      window.data.Notice.priceElement.min = typeMinPrices[2].price;
-    } else if (window.data.Notice.typeElement.value === typeMinPrices[3].type) {
-      window.data.Notice.priceElement.min = typeMinPrices[3].price;
+    if (window.data.notice.typeElement.value === typeMinPrices[1].type) {
+      window.data.notice.priceElement.min = typeMinPrices[1].price;
+    } else if (window.data.notice.typeElement.value === typeMinPrices[2].type) {
+      window.data.notice.priceElement.min = typeMinPrices[2].price;
+    } else if (window.data.notice.typeElement.value === typeMinPrices[3].type) {
+      window.data.notice.priceElement.min = typeMinPrices[3].price;
     } else {
-      window.data.Notice.priceElement.min = typeMinPrices[0].price;
+      window.data.notice.priceElement.min = typeMinPrices[0].price;
     }
-    handleInvalidity(evt);
+    manageInvalidity(evt);
   };
 
   var timeInClickHandler = function () {
-    window.data.Notice.timeOutElement.value = window.data.Notice.timeInElement.value;
-    window.data.Notice.timeInElement.title = 'Обратите внимание: при изменении времени заезда меняется время выезда и наоборот';
+    window.data.notice.timeOutElement.value = window.data.notice.timeInElement.value;
+    window.data.notice.timeInElement.title = 'При изменении времени ЗАЕЗДА меняется время выезда и наоборот';
   };
 
   var timeOutClickHandler = function () {
-    window.data.Notice.timeInElement.value = window.data.Notice.timeOutElement.value;
-    window.data.Notice.timeOutElement.title = 'Обратите внимание: при изменении времени выезда меняется время заезда и наоборот';
+    window.data.notice.timeInElement.value = window.data.notice.timeOutElement.value;
+    window.data.notice.timeOutElement.title = 'При изменении времени ВЫЕЗДА меняется время заезда и наоборот';
   };
 
   var submitInvalidClickHandler = function () {
     roomsCapacityClickHandler();
-    var invalidCollection = document.querySelectorAll('input:invalid, select:invalid');
-    for (var i = 0; i < invalidCollection.length; i++) {
-      invalidCollection[i].classList.add('bordered');
+    var invalidFields = document.querySelectorAll('input:invalid, select:invalid');
+    for (var i = 0; i < invalidFields.length; i++) {
+      invalidFields[i].classList.add('bordered');
     }
   };
 
-  var resetClickHandler = function () {
+  var resetClickHandler = function (evt) {
+    evt.preventDefault();
+    removeBorder();
     window.utilities.disableNoticeForm();
   };
 
@@ -92,19 +105,19 @@
     }
   };
 
-  window.data.Notice.submitBtnElement.addEventListener('click', submitInvalidClickHandler);
-  window.data.Notice.capacityElement.addEventListener('click', capacityClickHandler);
-  window.data.Notice.capacityElement.addEventListener('click', roomsCapacityClickHandler);
-  window.data.Notice.roomNumberElement.addEventListener('click', roomsCapacityClickHandler);
-  window.data.Notice.titleElement.addEventListener('click', titleClickHandler);
-  window.data.Notice.priceElement.addEventListener('click', typePriceClickHandler);
-  window.data.Notice.typeElement.addEventListener('click', typePriceClickHandler);
-  window.data.Notice.timeInElement.addEventListener('click', timeInClickHandler);
-  window.data.Notice.timeOutElement.addEventListener('click', timeOutClickHandler);
-  window.data.Notice.resetElement.addEventListener('click', resetClickHandler);
-  window.data.Notice.resetElement.addEventListener('keypress', resetKeypressHandler);
-  window.data.Notice.formElement.addEventListener('submit', function (evt) {
+  window.data.notice.submitButtonElement.addEventListener('click', submitInvalidClickHandler);
+  window.data.notice.capacityElement.addEventListener('click', capacityClickHandler);
+  window.data.notice.capacityElement.addEventListener('click', roomsCapacityClickHandler);
+  window.data.notice.roomNumberElement.addEventListener('click', roomsCapacityClickHandler);
+  window.data.notice.titleElement.addEventListener('click', titleClickHandler);
+  window.data.notice.priceElement.addEventListener('click', typePriceClickHandler);
+  window.data.notice.typeElement.addEventListener('click', typePriceClickHandler);
+  window.data.notice.timeInElement.addEventListener('click', timeInClickHandler);
+  window.data.notice.timeOutElement.addEventListener('click', timeOutClickHandler);
+  window.data.notice.resetElement.addEventListener('click', resetClickHandler);
+  window.data.notice.resetElement.addEventListener('keypress', resetKeypressHandler);
+  window.data.notice.formElement.addEventListener('submit', function (evt) {
     evt.preventDefault();
-    window.backend.send(new FormData(window.data.Notice.formElement), window.utilities.successSendHandler, window.utilities.errorSendHandler);
+    window.backend.send(new FormData(window.data.notice.formElement), window.utilities.successSendHandler, window.utilities.errorSendHandler);
   });
 })();
