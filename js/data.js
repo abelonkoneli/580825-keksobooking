189d69bb6aa22999349fe11db2058cb1485ef38f
abelonkoneli <abelonkoneli@gmail.com>
2018-03-04 18:56:30 +0200

    Правки по критериям

--- js/data.js
+++ js/data.js
@@ -7,18 +7,24 @@
       ESC: 27,
       ENTER: 13
     },
+    pinMainValues: {
+      WIDTH: 65,
+      HEIGHT: 65,
+      AFTER_HEIGHT: 18,
+      Z_INDEX: 999
+    },
     mapElement: document.querySelector('.map'),
     pinMainElement: document.querySelector('.map__pin--main'),
     mapFiltersElement: document.querySelector('.map__filters'),
-    Notice: {
+    notice: {
       formElement: document.querySelector('.notice__form'),
-      collection: document.querySelectorAll('fieldset:not(#housing-features)'),
+      fieldsets: document.querySelectorAll('fieldset:not(#housing-features)'),
       addressElement: document.querySelector('#address'),
       timeInElement: document.querySelector('#timein'),
       timeOutElement: document.querySelector('#timeout'),
       roomNumberElement: document.querySelector('#room_number'),
       resetElement: document.querySelector('.form__reset'),
-      submitBtnElement: document.querySelector('.form__submit'),
+      submitButtonElement: document.querySelector('.form__submit'),
       typeElement: document.querySelector('#type'),
       capacityElement: document.querySelector('#capacity'),
       priceElement: document.querySelector('#price'),
