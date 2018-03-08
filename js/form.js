1243caad991496308f26811fb2f2a8b8668193bf
abelonkoneli <abelonkoneli@gmail.com>
2018-03-08 13:05:13 +0200

    Handlers editing

--- js/form.js
+++ js/form.js
@@ -12,12 +12,13 @@
     }
   };
 
-  var titleClickHandler = function (evt) {
+  var titleChangeHandler = function (evt) {
     manageInvalidity(evt);
   };
 
-  var capacityClickHandler = function (evt) {
+  var capacityChangeHandler = function (evt) {
     manageInvalidity(evt);
+    window.data.notice.capacityElement.setCustomValidity('');
   };
 
   var removeBorder = function () {
@@ -27,7 +28,7 @@
     }
   };
 
-  var typePriceClickHandler = function (evt) {
+  var setTypeAndPriceCorrelation = function () {
     var typeMinPrices = [
       {
         type: 'bungalo',
@@ -54,16 +55,23 @@
     } else if (window.data.notice.typeElement.value === typeMinPrices[3].type) {
       window.data.notice.priceElement.min = typeMinPrices[3].price;
     }
+  };
 
+  var typeChangeHandler = function () {
+    setTypeAndPriceCorrelation();
+  };
+
+  var priceChangeHandler = function (evt) {
+    setTypeAndPriceCorrelation();
     manageInvalidity(evt);
   };
 
-  var timeInClickHandler = function () {
+  var timeInChangeHandler = function () {
     window.data.notice.timeOutElement.value = window.data.notice.timeInElement.value;
     window.data.notice.timeInElement.title = 'При изменении времени ЗАЕЗДА меняется время выезда и наоборот';
   };
 
-  var timeOutClickHandler = function () {
+  var timeOutChangeHandler = function () {
     window.data.notice.timeInElement.value = window.data.notice.timeOutElement.value;
     window.data.notice.timeOutElement.title = 'При изменении времени ВЫЕЗДА меняется время заезда и наоборот';
   };
@@ -74,23 +82,14 @@
     window.utilities.disableNoticeForm();
   };
 
-  var resetKeypressHandler = function (evt) {
-    if (evt.keycode === window.data.keycode.ENTER) {
-      resetClickHandler();
-    }
-  };
-
   window.data.notice.submitButtonElement.addEventListener('click', window.utilities.submitInvalidClickHandler);
-  window.data.notice.capacityElement.addEventListener('click', capacityClickHandler);
-  window.data.notice.capacityElement.addEventListener('click', window.utilities.roomsCapacityClickHandler);
-  window.data.notice.roomNumberElement.addEventListener('click', window.utilities.roomsCapacityClickHandler);
-  window.data.notice.titleElement.addEventListener('click', titleClickHandler);
-  window.data.notice.priceElement.addEventListener('click', typePriceClickHandler);
-  window.data.notice.typeElement.addEventListener('click', typePriceClickHandler);
-  window.data.notice.timeInElement.addEventListener('click', timeInClickHandler);
-  window.data.notice.timeOutElement.addEventListener('click', timeOutClickHandler);
+  window.data.notice.capacityElement.addEventListener('change', capacityChangeHandler);
+  window.data.notice.titleElement.addEventListener('change', titleChangeHandler);
+  window.data.notice.priceElement.addEventListener('change', priceChangeHandler);
+  window.data.notice.typeElement.addEventListener('change', typeChangeHandler);
+  window.data.notice.timeInElement.addEventListener('change', timeInChangeHandler);
+  window.data.notice.timeOutElement.addEventListener('change', timeOutChangeHandler);
   window.data.notice.resetElement.addEventListener('click', resetClickHandler);
-  window.data.notice.resetElement.addEventListener('keypress', resetKeypressHandler);
   window.data.notice.formElement.addEventListener('submit', function (evt) {
     evt.preventDefault();
     window.backend.send(new FormData(window.data.notice.formElement), window.utilities.successSendHandler, window.utilities.errorSendHandler);
