a1b2c251812ec5e114ff5e8629107cc5621134a1
abelonkoneli <abelonkoneli@gmail.com>
2018-03-08 13:01:55 +0200

    Constants to the top, changing the coordinates calc

--- js/button.js
+++ js/button.js
@@ -2,13 +2,12 @@
 'use strict';
 
 (function () {
-  var buttonElementTemplate = document.querySelector('template').content.querySelector('.map__pin');
-  var BUTTON_WIDTH = 50;
   var BUTTON_HEIGHT = 70;
+  var buttonElementTemplate = document.querySelector('template').content.querySelector('.map__pin');
   window.createButtonElement = function (proposal) {
     var buttonElement = buttonElementTemplate.cloneNode(true);
-    buttonElement.style.left = (proposal.location.x - BUTTON_WIDTH / 2) + 'px';
-    buttonElement.style.top = (proposal.location.y - BUTTON_HEIGHT) + 'px';
+    buttonElement.style.left = proposal.location.x + 'px';
+    buttonElement.style.top = (proposal.location.y - BUTTON_HEIGHT / 2) + 'px';
     buttonElement.querySelector('img').src = proposal.author.avatar;
     return buttonElement;
   };
